a7ebbf593da5f2b6cf5213df2c40336eabfea545
Zdeněk Novotný (DeznekCZ) <zde.nov.chr@seznam.cz>
2025-03-24 17:51:24 +0100

    Other changes

---
+++ src/Bridges/Data/Translate/NewTr.cs
@@ -0,0 +1,14 @@
+﻿using Mafi.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramableNetwork
+{
+    public partial class NewTr
+    {
+        
+    }
+}
--- src/ExampleMod.Unity/Assets/DLLs/create_dll_hardlinks.bat
+++ src/ExampleMod.Unity/Assets/DLLs/create_dll_hardlinks.bat
@@ -9,3 +9,6 @@ for %%s in ("Mafi.dll" "Mafi.Core.dll" "Mafi.Base.dll" "Mafi.Unity.dll", "Mafi.M
 	IF EXIST "%~dp0\%%s" del "%~dp0\%%s"
 	mklink "%~dp0\%%s" "%COI_ROOT%\Captain of Industry_Data\Managed\%%s"
 )
+
+IF EXIST "%~dp0\ProgramableNetwork.dll" del "%~dp0\ProgramableNetwork.dll"
+mklink "%~dp0\ProgramableNetwork.dll" "S:\git\COIM-Derbis\src\ProgramableNetwork\bin\Debug\net471\ProgramableNetwork.dll"
