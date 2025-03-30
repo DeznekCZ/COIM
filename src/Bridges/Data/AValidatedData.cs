3219a8bea061e85e744f16aabe55c79f8b1ec1dd
Zdeněk Novotný (DeznekCZ) <zde.nov.chr@seznam.cz>
2025-03-30 17:27:10 +0200

    base stuff

---
+++ src/Bridges/Data/AValidatedData.cs
@@ -0,0 +1,24 @@
+﻿using Mafi;
+using Mafi.Core.Mods;
+using System;
+
+namespace ProgramableNetwork
+{
+    public abstract class AValidatedData : IModData
+    {
+        public void RegisterData(ProtoRegistrator registrator)
+        {
+            try
+            {
+                RegisterDataInternal(registrator);
+            }
+            catch (System.Exception e)
+            {
+                Log.Exception(e);
+                throw e;
+            }
+        }
+
+        protected abstract void RegisterDataInternal(ProtoRegistrator registrator);
+    }
+}
